ed6d6c234e60f08d51a632eae4e5ec841bd0cafe
unknown <jackellis.homme@gmail.com>
2019-11-19 17:55:54 -0500

    Working eigenface implementation

--- dataClean.py
+++ dataClean.py
@@ -16,14 +16,14 @@ for filename in os.listdir(imageDIR):
     personTags = filename.split('.')
     if len(personTags) == 3:
         personTag = int(personTags[1])
-        if personTag in [1,10,11,12,13]:
+        if personTag in [1,10,11,12,13,14,15,16,17,18]:
             flag = True
             path = os.path.join(imageDIR,filename)
             im = cv2.imread(path,0)
             im = cv2.resize(im,(100,100))
             newPath = os.path.join(newDIR,filename)
             cv2.imwrite(newPath,im)
-        elif personTag in [14]:
+        elif personTag in [19]:
             flag = True
             path = os.path.join(imageDIR,filename)
             im = cv2.imread(path,0)
